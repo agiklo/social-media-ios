885fec57644d2e0b143320878b66757e891fa5e7
agiklo <mateuszmilewczyk1@gmail.com>
2022-03-13 22:35:11 +0100

    Initial Commit

---
+++ social-media-ios/social_media_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  social_media_iosApp.swift
+//  social-media-ios
+//
+//  Created by Mateusz Milewczyk on 13/03/2022.
+//
+
+import SwiftUI
+
+@main
+struct social_media_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
